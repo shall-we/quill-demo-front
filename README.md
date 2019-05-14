3e47376197d94a80dcf9b640531ea9e0d4830c9c
corock <hoongineer@gmail.com>
2019-05-14 09:49:36 +0900

    Add comment

--- README.md
+++ README.md
@@ -2,10 +2,10 @@
 
 ## Installing and Usage
 ```shell
-git clone
+git clone https://github.com/shall-we/quill-demo-front.git
 cd quill-demo-front
 yarn install
 yarn start
 ```
-You can also `install` and start using npm instead of `yarn`.
+You can also `install` and start using npm instead of `yarn`.  
 And then connect to `localhost:3000`.
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Editor from './Editor';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import Editor from "./Editor";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<Editor />, document.getElementById('root'));
+ReactDOM.render(<Editor />, document.getElementById("root"));
 serviceWorker.unregister();
